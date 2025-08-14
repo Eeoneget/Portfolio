86f361ac07c2875cbdfac0029fa8e10249b3f2f0
Eeoneget <esengalidanial@gmail.com>
2025-08-15 02:03:14 +0500

    eshkere

---
+++ src/Components/Nav.tsx
@@ -0,0 +1,32 @@
+import { motion } from 'framer-motion'
+
+function Nav({textEnter, textLeave, backgroundState}: {textEnter: () => void, textLeave: () => void, backgroundState: string}) {
+
+
+    return ( 
+        <>
+            <nav className="nav">
+                <motion.div className="container flex-split-horizontal"
+                    initial={{
+                        transform: "translateY(-60px)"
+                    }}
+                    animate={{
+                        transform: "translateY(0px)",
+                        transition: {
+                            delay: 0.5,
+                            duration: 0.7,
+                        }
+                    }}
+                    >
+                    <a href="#" data-state={backgroundState} onMouseEnter={textEnter} onMouseLeave={textLeave} className="logo" title='home'>D<span className="clr-neutral-beige">C</span></a>
+                    <div className="link-parent">
+                        <a href="https://github.com/eeoneget" data-state={backgroundState} target="_blank" onMouseEnter={textEnter} onMouseLeave={textLeave} className="link-block"><span className="fs-larger">G</span>itHub</a>
+                        <a href="mailto:esengalidanial@gmail.com" title="email" data-state={backgroundState} onMouseEnter={textEnter} onMouseLeave={textLeave} className="link-block"><span className="fs-larger">C</span>ontact</a>
+                    </div>
+                </motion.div>
+            </nav>
+        </>
+     );
+}
+
+export default Nav;
